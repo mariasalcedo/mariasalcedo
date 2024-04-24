b580aadca6906a8e62ea7ff5cab93f3170ef9383
Maria Salcedo <mane.salcedo.gitcommit@gmail.com>
2024-04-24 15:30:03 +0200

    feat(workflows): Automate blog post updates on README (#1)

---
+++ update/PREREADME.md
@@ -0,0 +1,6 @@
+
+  <!--<a href="https://www.masagu.dev" target="_blank" rel="noreferrer"><img align="center" alt="Maria Salcedo logo" src="https://masagu.mo.cloudinary.net/logo.png" width="200" /></a> -->
+
+## Projects
+
+- Freelancing :globe_with_meridians: [www.masagu.dev](https://www.masagu.dev)
---
+++ update/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"github.com/mmcdole/gofeed"
+	"io"
+	"log"
+	"os"
+)
+
+func makeReadme(filename string) error {
+	fp := gofeed.NewParser()
+	feed, err := fp.ParseURL("https://masagu.dev/index.xml")
+	if err != nil {
+		log.Fatalf("error getting feed: %v", err)
+	}
+
+	blogItem := feed.Items[0]
+
+	pre, err := readPrecookedReadme("./PREREADME.md")
+	blog := "- Latest blog post :page_facing_up: [" + blogItem.Title + "](" + blogItem.Link + ")"
+	post, err := readPrecookedReadme("./POSTREADME.md")
+	data := fmt.Sprintf("%s\n%s\n%s", pre, blog, post)
+
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = io.WriteString(file, data)
+	if err != nil {
+		return err
+	}
+	return file.Sync()
+}
+
+func readPrecookedReadme(filename string) (string, error) {
+	dat, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatalf("error reading file: %s %v", filename, err)
+	}
+	return string(dat), nil
+}
+
+func main() {
+	makeReadme("../README.md")
+}
